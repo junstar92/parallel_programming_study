6b0feca8249a9389f3a156e31aeeb8c1d6f89cf1
changjun.lee <pord7457@gmail.com>
2021-12-28 23:45:58 +0900

    add function declaration

--- CUDA/BFS/bfs.cu
+++ CUDA/BFS/bfs.cu
@@ -28,6 +28,7 @@ bool checkValid(int* cpu_output, int* gpu_output, int numVertex);
 void BFS_sequential(int source, int* edges, int* dest, int* label);
 void BFS(int source, int* adj, int numVertex, int* label);
 void BFS_host(int source, int* h_edges, int* h_dest, int* h_label, int numVertex, int numNonzero);
+__global__ void BFS_Bqueue_kernel(int* p_frontier, int* p_frontier_tail, int* c_frontier, int* c_frontier_tail, int* edges, int* dest, int* label, int* visited);
 
 int main(int argc, char** argv)
 {
@@ -249,47 +250,6 @@ void BFS(int source, int* adj, int numVertex, int* label)
     free(visited);
 }
 
-__global__
-void BFS_Bqueue_kernel(int* p_frontier, int* p_frontier_tail, int* c_frontier, int* c_frontier_tail, int* edges, int* dest, int* label, int* visited)
-{
-    __shared__ int c_frontier_s[BLOCK_QUEUE_SIZE];
-    __shared__ int c_frontier_tail_s, our_c_frontier_tail;
-
-    if (threadIdx.x == 0)
-        c_frontier_tail_s = 0;
-    __syncthreads();
-
-    const int tid = blockIdx.x*blockDim.x + threadIdx.x;
-    if (tid < *p_frontier_tail) {
-        const int my_vertex = p_frontier[tid];
-        for (int i = edges[my_vertex]; i < edges[my_vertex+1]; i++) {
-            const int was_visited = atomicExch(&(visited[dest[i]]), 1);
-            if (!was_visited) {
-                label[dest[i]] = label[my_vertex] + 1;
-                const int my_tail = atomicAdd(&c_frontier_tail_s, 1);
-                if (my_tail < BLOCK_QUEUE_SIZE) {
-                    c_frontier_s[my_tail] = dest[i];
-                }
-                else {
-                    c_frontier_tail_s = BLOCK_QUEUE_SIZE;
-                    const int my_global_tail = atomicAdd(c_frontier_tail, 1);
-                    c_frontier[my_global_tail] = dest[i];
-                }
-            }
-        }
-    }
-    __syncthreads();
-
-    if (threadIdx.x == 0) {
-        our_c_frontier_tail = atomicAdd(c_frontier_tail, c_frontier_tail_s);
-    }
-    __syncthreads();
-
-    for (int i = threadIdx.x; i < c_frontier_tail_s; i++) {
-        c_frontier[our_c_frontier_tail + i] = c_frontier_s[i];
-    }
-}
-
 void BFS_host(int source, int* h_edges, int* h_dest, int* h_label, int numVertex, int numNonzero)
 {
     // host memory
@@ -360,4 +320,45 @@ void BFS_host(int source, int* h_edges, int* h_dest, int* h_label, int numVertex
     CUDA_CHECK(cudaFree(d_frontier));
     CUDA_CHECK(cudaFree(d_c_frontier_tail));
     CUDA_CHECK(cudaFree(d_p_frontier_tail));
+}
+
+__global__
+void BFS_Bqueue_kernel(int* p_frontier, int* p_frontier_tail, int* c_frontier, int* c_frontier_tail, int* edges, int* dest, int* label, int* visited)
+{
+    __shared__ int c_frontier_s[BLOCK_QUEUE_SIZE];
+    __shared__ int c_frontier_tail_s, our_c_frontier_tail;
+
+    if (threadIdx.x == 0)
+        c_frontier_tail_s = 0;
+    __syncthreads();
+
+    const int tid = blockIdx.x*blockDim.x + threadIdx.x;
+    if (tid < *p_frontier_tail) {
+        const int my_vertex = p_frontier[tid];
+        for (int i = edges[my_vertex]; i < edges[my_vertex+1]; i++) {
+            const int was_visited = atomicExch(&(visited[dest[i]]), 1);
+            if (!was_visited) {
+                label[dest[i]] = label[my_vertex] + 1;
+                const int my_tail = atomicAdd(&c_frontier_tail_s, 1);
+                if (my_tail < BLOCK_QUEUE_SIZE) {
+                    c_frontier_s[my_tail] = dest[i];
+                }
+                else {
+                    c_frontier_tail_s = BLOCK_QUEUE_SIZE;
+                    const int my_global_tail = atomicAdd(c_frontier_tail, 1);
+                    c_frontier[my_global_tail] = dest[i];
+                }
+            }
+        }
+    }
+    __syncthreads();
+
+    if (threadIdx.x == 0) {
+        our_c_frontier_tail = atomicAdd(c_frontier_tail, c_frontier_tail_s);
+    }
+    __syncthreads();
+
+    for (int i = threadIdx.x; i < c_frontier_tail_s; i++) {
+        c_frontier[our_c_frontier_tail + i] = c_frontier_s[i];
+    }
 }
