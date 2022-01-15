89c19adc3f8bbf135b95e54cd4c3732f388e74b9
changjun.lee <pord7457@gmail.com>
2022-01-15 15:25:44 +0900

    change folder name and update code without unified memory

--- CUDA/UnifiedMemory/matrixAddWithoutUnifiedMemory.cu
+++ CUDA/UnifiedMemory/matrixAddWithoutUnifiedMemory.cu
@@ -82,17 +82,17 @@ int main(int argc, char** argv)
     printf("Matrix size: nx %d ny %d\n", nx, ny);
 
     // malloc host memory
-    float *A, *B, *hostRef, *gpuRef;
-    CUDA_CHECK(cudaMallocManaged((void**)&A, nBytes));
-    CUDA_CHECK(cudaMallocManaged((void**)&B, nBytes));
-    CUDA_CHECK(cudaMallocManaged((void**)&hostRef, nBytes));
-    CUDA_CHECK(cudaMallocManaged((void**)&gpuRef, nBytes));
+    float *h_A, *h_B, *hostRef, *gpuRef;
+    h_A = (float*)malloc(nBytes);
+    h_B = (float*)malloc(nBytes);
+    hostRef = (float*)malloc(nBytes);
+    gpuRef = (float*)malloc(nBytes);
 
     double start, finish;
     // initialize data at host side
     GET_TIME(start);
-    initialData(A, nxy);
-    initialData(B, nxy);
+    initialData(h_A, nxy);
+    initialData(h_B, nxy);
     GET_TIME(finish);
     printf("initialization: \t %f sec\n", finish - start);
 
@@ -105,30 +105,49 @@ int main(int argc, char** argv)
     GET_TIME(finish);
     printf("sumMatrix on host:\t %f sec\n", finish - start);
 
-    // invode kernel at host side
+    // malloc device global memory
+    float *d_A, *d_B, *d_C;
+    CUDA_CHECK(cudaMalloc((void**)&d_A, nBytes));
+    CUDA_CHECK(cudaMalloc((void**)&d_B, nBytes));
+    CUDA_CHECK(cudaMalloc((void**)&d_C, nBytes));
+
+    // invoke kernel at host side
     int dimX = 32;
     int dimY = 32;
     dim3 blocks(dimX, dimY);
     dim3 grids((nx + blocks.x - 1) / blocks.x, (ny + blocks.y - 1) / blocks.y);
     
     // warm-up kernel
-    sumMatrixOnGPU<<<grids, blocks>>>(A, B, gpuRef, nx, ny);
+    CUDA_CHECK(cudaMemset(d_A, 0.0f, nBytes));
+    CUDA_CHECK(cudaMemset(d_B, 0.0f, nBytes));
+    sumMatrixOnGPU<<<grids, blocks>>>(d_A, d_B, d_C, nx, ny);
     CUDA_CHECK(cudaDeviceSynchronize());
 
+    // transfer data from host to device
+    CUDA_CHECK(cudaMemcpy(d_A, h_A, nBytes, cudaMemcpyHostToDevice));
+    CUDA_CHECK(cudaMemcpy(d_B, h_B, nBytes, cudaMemcpyHostToDevice));
+
     GET_TIME(start);
-    sumMatrixOnGPU<<<grids, blocks>>>(A, B, gpuRef, nx, ny);
+    sumMatrixOnGPU<<<grids, blocks>>>(d_A, d_B, d_C, nx, ny);
     CUDA_CHECK(cudaDeviceSynchronize());
     GET_TIME(finish);
     printf("sumMatrix on gpu :\t %f sec <<<(%d,%d), (%d,%d)>>>\n", finish-start, grids.x, grids.y, blocks.x, blocks.y);
+
+    CUDA_CHECK(cudaMemcpy(gpuRef, d_C, nBytes, cudaMemcpyDeviceToHost));
 
     // check device results
     checkResult(hostRef, gpuRef, nxy);
 
     // free device global memory
-    CUDA_CHECK(cudaFree(A));
-    CUDA_CHECK(cudaFree(B));
-    CUDA_CHECK(cudaFree(hostRef));
-    CUDA_CHECK(cudaFree(gpuRef));
+    CUDA_CHECK(cudaFree(d_A));
+    CUDA_CHECK(cudaFree(d_B));
+    CUDA_CHECK(cudaFree(d_C));
+
+    // free host memory
+    free(h_A);
+    free(h_B);
+    free(hostRef);
+    free(gpuRef);
 
     CUDA_CHECK(cudaDeviceReset());
     return 0;
